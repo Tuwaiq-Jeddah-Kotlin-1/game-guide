0a6a740521435cf1692e323860a0bb1786856eb2
badr <badr.fadul.jed.k1@tuwaiq.edu.sa>
2021-12-16 04:27:42 +0300

    adding bottom navigation bar

--- app/src/main/java/com/example/gameguide/MainActivity.kt
+++ app/src/main/java/com/example/gameguide/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.gameguide
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -26,6 +31,33 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         navController = navHostFragment.navController
+
+
+        /*navController = findNavController(R.id.fragmentContainerView)*/
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+
+        bottomNavigationView.setupWithNavController(navController)
+
+        /*navController.popBackStack(R.id.homepage, false)*/
+
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.forgetPassword -> {
+                    bottomNavigationView.visibility = View.GONE
+                }
+                R.id.signIn -> {
+                    bottomNavigationView.visibility = View.GONE
+                }
+                R.id.registration -> {
+                    bottomNavigationView.visibility = View.GONE
+                }
+                else -> {
+                    bottomNavigationView.visibility = View.VISIBLE
+                }
+            }
+        }
 
         /*setupActionBarWithNavController(navController)
     }
